519dc7246cf7e9dc1794fd5a6827ca9a91a4bc57
Yaroslav Dashkevich <y.dashkevich@pm.me>
2020-09-04 14:58:27 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 
-class App extends React.Component {
-  state = {
-    value: localStorage.getItem("info") || ""
+const App: React.FC<{}> = () => {
+  const [value, setValue] = useState(localStorage.getItem("info") || "");
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    localStorage.setItem("info", value);
+    setValue(value);
   };
 
-  componentDidUpdate() {
-    localStorage.setItem("info", this.state.value);
-  }
-
-  onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({value: event.target.value});
-  };
-
-  render() {
-    const {value} = this.state;
-    return (
-      <div>
-        <input value={value} type="text" onChange={this.onChange} />
-        <p>{value}</p>
-      </div>
-    );
-  }
+  return <div>
+    <input value={value} type="text" onChange={handleChange} />
+    <p>{value}</p>
+  </div>;
 };
 
 export default App;
